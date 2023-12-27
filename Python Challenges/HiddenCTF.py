d0cbd4de94b5753e5f17994b5353351e2a00e1fc
tomdoescode <tomsayers88@googlemail.com>
2023-12-27 12:22:16 +0000

    HiddenCTF
    Find the hidden CTF in this Python code

---
+++ Python Challenges/HiddenCTF.py
@@ -0,0 +1,23 @@
+def process_strings(str0, str1, str2, str3, str4, str5, str6, str7, str8, str9, str10):
+    # Perform some processing on the input strings
+    processed_output = f"Processed: {str0} {str1} {str2} {str3} {str4} {str5} {str6} {str7} {str8} {str9} {str10}"
+    
+    return processed_output
+
+# Example usage
+input_str0 = "73 6f 6c 6f 43 54 46"
+input_str1 = "7b 30 39 53 4b 5f"
+input_str2 = "30 39 53 4b 5f"
+input_str3 = "39 53 4d 39 4d 34 5f"
+input_str4 = "44 4d 44 39 34 4b 39 5f"
+input_str5 = "34 30 52 4b 52 39 34 5f"
+input_str6 = "49 52 4a 46 39 34 5f 20"
+input_str7 = "4a 44 39 34 4d 52 4f 46 5f"
+input_str8 = "34 30 52 4b 45 52 30 34 5f"
+input_str9 = "44 30 44 4b 44 39 30 34 33 5f"
+input_str10 = "34 4b 46 39 46 4f 34 4b 7d"
+
+output_result = process_strings(input_str0, input_str1, input_str2, input_str3, input_str4, input_str5, input_str6, input_str7, input_str8, input_str9, input_str10)
+
+# Display the output (Hidden)
+# print(output_result)
